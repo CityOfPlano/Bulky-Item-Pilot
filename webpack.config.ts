2587342797134b0361521d6148e12e5bdf4d5e4e
Gordon Goodrum <stopgordy@gmail.com>
2020-02-27 12:14:03 -0600

    webpack build desc

---
+++ webpack.config.ts
@@ -0,0 +1,34 @@
+const nodeExternals = require('webpack-node-externals');
+const path = require('path');
+
+const common = {
+
+    resolve: {
+        extensions: ['.js', '.jsx'] // common extensions
+    }
+    // other plugins, postcss config etc. common for frontend and backend
+};
+
+const backend = {
+    entry: [
+        './tmp/service/BulkyItemsPickupUtilityRoutingService.js'
+    ],
+    output: {
+        path: path.join(__dirname, 'artifact'),
+        libraryTarget: "var",
+        filename: 'bundle-backend.js'
+    },
+    target: 'node',
+    node: {
+        // Need this when working with express, otherwise the build fails
+        __dirname: false,   // if you don't put this is, __dirname
+        __filename: false,  // and __filename return blank or /
+    },
+    externals: [nodeExternals({
+        whitelist: ['jquery', 'chai']
+    })]
+};
+
+module.exports = [
+    Object.assign({} , common, backend)
+];
