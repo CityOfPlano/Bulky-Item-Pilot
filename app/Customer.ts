2199fe9b2669a38d1c02352121c4c8afb4428332
Gordon Goodrum <stopgordy@gmail.com>
2020-03-03 13:14:10 -0600

    Wizard Client State

--- app/Customer.ts
+++ app/Customer.ts
@@ -3,9 +3,11 @@ import {StepLoading} from "./wizard/StepLoading";
 import {StepLanding} from "./wizard/StepLanding";
 import {StepCustomerAuth} from "./wizard/StepCustomerAuth";
 import {DomWizardRenderer} from "../lib/controller/DomWizardRenderer";
+import {ClientWizardState} from "../lib/WizardState";
 
 let wizard_renderer = new DomWizardRenderer();
 let wizard = new Wizard(document.body, wizard_renderer);
+wizard.useState(new ClientWizardState());
 
 let step_loading = new StepLoading();
 
