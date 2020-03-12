641da92cb5fb35c21977b34a6869db7e9acd03ec
Gordon Goodrum <stopgordy@gmail.com>
2020-03-12 10:52:17 -0500

    Calendar Spec

--- lib/controller/Calendar.spec.ts
+++ lib/controller/Calendar.spec.ts
@@ -3,7 +3,7 @@ import {Calendar} from "./Calendar";
 
 describe('Calendar', () => {
 
-    let calendar = new Calendar();
+    let calendar = new Calendar("02/1/1992");
 
     it('should be created', () => {
         expect(calendar).not.equal(undefined);
@@ -14,15 +14,34 @@ describe('Calendar', () => {
         expect(calc.month).equal(0);
         expect(calc.year).equal(2020);
     });
+
     it('should calculate month prev year', () => {
         let calc = calendar.getCalculatedMonthYear(-2,2020);
         expect(calc.month).equal(10);
         expect(calc.year).equal(2019);
     });
+
     it('should calculate month next year', () => {
         let calc = calendar.getCalculatedMonthYear(12,2020);
         expect(calc.month).equal(0);
         expect(calc.year).equal(2021);
+    });
+
+    it('should return correct datestring', () => {
+        let calc = calendar.getSelectedAsString();
+        expect(calc).equal("2/1/1992");
+    });
+
+    it('should return correct number of days', () => {
+        let calc = calendar.getDaysInMonth(0 /* January = 1 */,2020);
+        expect(calc).equal(31);
+        calc = calendar.getDaysInMonth(1 /* February = 1 */,2019);
+        expect(calc).equal(28);
+    });
+
+    it('should return correct number of days leap year', () => {
+        let calc = calendar.getDaysInMonth(1 /* February = 1 */,2020);
+        expect(calc).equal(29);
     });
 
 });
